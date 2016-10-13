682ec46ed44f31e47febd4adf6b61ffbb9ddd7c7
fbrigatt <fbrigatt@users.noreply.github.com>
2016-10-13 18:58:36 -0300

    Update Teste.java

--- Teste/src/teste/Teste.java
+++ Teste/src/teste/Teste.java
@@ -15,7 +15,6 @@ public class Teste {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Teste Git");
     }
     
